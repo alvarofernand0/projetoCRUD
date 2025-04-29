dc0909bebdd5949a0c2e7326ec5ceca900adf1c5
alvarofernand0 <alvarofernando1324@gmail.com>
2025-04-29 16:36:25 -0300

    Iniciando a estrutura básica do CRUD

--- projetoCRUD/Program.cs
+++ projetoCRUD/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Projeto CRUD - Clinica Multi-Profissional
 
 using System;
+//using System.Collections.Generic;
 
 class Program
 {
@@ -9,28 +10,56 @@ class Program
         Console.WriteLine("\n=== MENU PRINCIPAL ===");
         Console.WriteLine("\n1 - Cadastrar Profissional");
         Console.WriteLine("2 - Listar Profissionais");
-        Console.WriteLine("3 - Atualizar profissioal");
+        Console.WriteLine("3 - Atualizar profissional");
         Console.WriteLine("4 - Excluir Profissional");
         Console.WriteLine("5 - SAIR");
         Console.WriteLine("Escolha uma das opções: ");
 
-        int escolha = int.Parse(Console.ReadLine());
-
-        switch (escolha)
+        int escolha;
+        if (!int.TryParse(Console.ReadLine(), out escolha))
         {
-            case 1:
-                Console.WriteLine("CADASTRANDO PROFISSIONAL");
-                break;
+           Console.WriteLine("Opção Inválida!");
+        }
+       Console.Clear();
 
-            case 2: 
-                Console.WriteLine("LISTAR PROFISSIONAIS"); 
-                break;
+        while (true)
+        try
+        {
+                if (escolha == 1)
+                {
+                    Console.WriteLine("\n=== Cadastrando Profissional ===\n");
 
+                }
+                else if (escolha == 2)
+                {
+                    Console.WriteLine("=== Listando Profissionais ===\n");
+                }
+                else if (escolha == 3)
+                {
+                    Console.WriteLine("=== Atualizando Profissional ===\n");
+                }
+                else if (escolha == 4)
+                {
+                    Console.WriteLine("=== Excluindo Profissional ===\n");
+                }
+                else if(escolha == 5)
+                {
+                    Console.WriteLine("Saindo da Aplicação...");
+
+                }else Console.WriteLine("Opção Inválida!"); break;
+
+        }
+        catch (Exception u)
+        {
+           Console.WriteLine("Opção Inválida!");
 
         }
 
-
+        
 
     }
-
+    static List<int> Id = new List<int>();
+    static List<string> Nome = new List<string>();
+    static List<string> Especialidade = new List<string>();
+    static List<int> Telefone = new List<int>();
 }
