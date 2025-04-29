5d3040f2c6177c12f127d221903857ed3564bfe6
alvarofernand0 <alvarofernando1324@gmail.com>
2025-04-29 12:22:07 -0300

    primeiro commit

---
+++ projetoCRUD/Program.cs
@@ -0,0 +1,36 @@
+﻿// Projeto CRUD - Clinica Multi-Profissional
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n=== MENU PRINCIPAL ===");
+        Console.WriteLine("\n1 - Cadastrar Profissional");
+        Console.WriteLine("2 - Listar Profissionais");
+        Console.WriteLine("3 - Atualizar profissioal");
+        Console.WriteLine("4 - Excluir Profissional");
+        Console.WriteLine("5 - SAIR");
+        Console.WriteLine("Escolha uma das opções: ");
+
+        int escolha = int.Parse(Console.ReadLine());
+
+        switch (escolha)
+        {
+            case 1:
+                Console.WriteLine("CADASTRANDO PROFISSIONAL");
+                break;
+
+            case 2: 
+                Console.WriteLine("LISTAR PROFISSIONAIS"); 
+                break;
+
+
+        }
+
+
+
+    }
+
+}
