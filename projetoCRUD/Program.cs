87946fa56acb30017c78d778e7342ebcbb4fab58
alvarofernand0 <alvarofernando1324@gmail.com>
2025-04-30 16:38:15 -0300

    novo commit

--- projetoCRUD/Program.cs
+++ projetoCRUD/Program.cs
@@ -8,61 +8,58 @@ class Program
     //Menu Principal
     static void Main(string[] args)
     {
-        Console.WriteLine("\n=== MENU PRINCIPAL ===");
-        Console.WriteLine("\n1 - Cadastrar Profissional");
-        Console.WriteLine("2 - Listar Profissionais");
-        Console.WriteLine("3 - Atualizar profissional");
-        Console.WriteLine("4 - Excluir Profissional");
-        Console.WriteLine("5 - SAIR");
-        Console.WriteLine("Escolha uma das opções: ");
-
-        int escolha;
-        if (!int.TryParse(Console.ReadLine(), out escolha))
+        while (true) // Esse while garante que o bloco abaixo seja executado enquanto não sair do programa
         {
-           Console.WriteLine("Opção Inválida!");
+            Console.Clear();
+            Console.WriteLine("\n=== MENU PRINCIPAL ===\n\n");
+            Console.WriteLine("1 - Cadastrar Profissional\n");
+            Console.WriteLine("2 - Listar Profissionais\n");
+            Console.WriteLine("3 - Atualizar profissional\n");
+            Console.WriteLine("4 - Excluir Profissional\n");
+            Console.WriteLine("5 - SAIR\n");
+            Console.WriteLine("Escolha uma das opções...1");
+
+            int escolha;
+            if (int.TryParse(Console.ReadLine(), out escolha)) // Caso False(0) pede para digitar opçao válida
+            {
+              
+            }
+
+            if (escolha == 1)
+            {
+                Console.WriteLine("\n=== Cadastrando Profissional ===\n");
+                Cadastrar();
+
+            }
+            else if (escolha == 2)
+            {
+                Console.WriteLine("=== Listando Profissionais ===\n");
+                Listar();
+            }
+            else if (escolha == 3)
+            {
+                Console.WriteLine("=== Atualizando Profissional ===\n");
+            }
+            else if (escolha == 4)
+            {
+                Console.WriteLine("=== Excluindo Profissional ===\n");
+            }
+            else if (escolha == 5)
+            {
+                Console.Clear();
+                Console.WriteLine("Saindo da Aplicação...");
+                break; // Para a execução caso escolha sair...
+            }
+            else
+            {
+                Console.WriteLine("Escolha uma opção válida...");
+            };
+           Console.ReadKey();
         }
-       Console.Clear();
-
-        while (true)
-        try
-        {
-                if (escolha == 1)
-                {
-                    Console.WriteLine("\n=== Cadastrando Profissional ===\n");
-                    Cadastrar();
-
-                }
-                else if (escolha == 2)
-                {
-                    Console.WriteLine("=== Listando Profissionais ===\n");
-                    Listar();
-                }
-                else if (escolha == 3)
-                {
-                    Console.WriteLine("=== Atualizando Profissional ===\n");
-                }
-                else if (escolha == 4)
-                {
-                    Console.WriteLine("=== Excluindo Profissional ===\n");
-                }
-                else if(escolha == 5)
-                {
-                    Console.WriteLine("Saindo da Aplicação...");
-
-                }else Console.WriteLine("Opção Inválida!"); break;
-
-        }
-        catch (Exception u)
-        {
-           Console.WriteLine("Opção Inválida!");
-
-        }
-
-        
-
     }
 
-    //Listas
+
+    // Lista para guardar os dados dos profissionais
 
     static List<int> registros = new List<int>();
     static List<string> nomes = new List<string>();
@@ -71,34 +68,44 @@ class Program
 
     static void Cadastrar()
     {
+        Console.Clear();
         Console.WriteLine("Digite o Nome:\n");
         string nome = Console.ReadLine();
 
         Console.WriteLine("Digite a Especialidade:\n");
-        string esp = Console.ReadLine();
+        string especialidade = Console.ReadLine();
 
         Console.WriteLine("Digite o Telefone: \n");
-        string tel = Console.ReadLine();
+        string telefone = Console.ReadLine();
 
-        int reg = registros.Count + 1;
+        int registro = registros.Count + 1;
 
-        registros.Add(reg);
+        registros.Add(registro);
         nomes.Add(nome);
-        especialidades.Add(esp);
-        telefones.Add(tel);
+        especialidades.Add(especialidade);
+        telefones.Add(telefone);
 
-        Console.WriteLine("O Profissional foi cadastrado com sucesso!");
+        Console.Clear();
+        Console.WriteLine("Cadastro Concluído!\nTecle 'C' para novo Cadastro ou 'Enter' para voltar ao Menu");
+
+        if (Console.ReadKey(true).Key == ConsoleKey.C)
+        {
+            Cadastrar();
+        }
+        
+        
     }
 
     static void Listar()
     {
+        Console.Clear();
         for (int i = 0; i < registros.Count; i++) 
         {
             Console.WriteLine($"Registro: {registros[i]}");
             Console.WriteLine($"Nome: {nomes[i]}");
             Console.WriteLine($"Especialidade: {especialidades[i]}");
             Console.WriteLine($"Telefone: {telefones[i]}");
-            
+
         }
     }
 }
