6a53e39ef870533128ba8b4f8fd17e8750299198
Alvaro <alvarofernando1324@gmail.com>
2025-05-01 23:34:26 -0300

    Fazendo melhorias no fluxo

--- projetoCRUD/Program.cs
+++ projetoCRUD/Program.cs
@@ -1,15 +1,21 @@
 ﻿// Projeto CRUD - Clinica Multi-Profissional
 
 using System;
-//using System.Collections.Generic;
+using System.Collections.Generic;
 
 class Program
 {
-    //Menu Principal
+    
     static void Main(string[] args)
     {
+        MenuPrincipal();
+    }
+
+    //Menu Principal
+    static void MenuPrincipal()
+    {
         while (true) // Esse While garante que o bloco abaixo seja
-                     // executado enquanto não escolher sair do programa
+                     // executado enquanto não escolher SAIR do programa
         {
             Console.Clear();
             Console.WriteLine("\n=== MENU PRINCIPAL ===\n\n");
@@ -19,52 +25,43 @@ class Program
             Console.WriteLine("4 - Excluir Profissional\n");
             Console.WriteLine("5 - SAIR\n");
             Console.WriteLine("Escolha uma das opções...");
-
+            
             int escolha;
             if (int.TryParse(Console.ReadLine(), out escolha)) // Caso a conversão retorne False(0)
-                                                               // Solicita para digitar opçao válida
-            {
-
-            }
-
+                                                               // Solicita para digitar opçao válida                                                   
             if (escolha == 1)
             {
-                Console.WriteLine("\n===# Cadastrando Profissional #===\n");
                 Cadastrar();
             }
             else if (escolha == 2)
             {
-                Console.WriteLine("===# Listando Profissionais #===\n");
                 Listar();
             }
             else if (escolha == 3)
             {
-                Console.WriteLine("===# Atualizando Profissional #===\n");
                 Atualizar();
             }
             else if (escolha == 4)
             {
-                Console.WriteLine("===# Excluindo Profissional #===\n");
                 Excluir();
             }
             else if (escolha == 5)
             {
                 Console.Clear();
                 Console.WriteLine("Saindo da Aplicação...");
-                break; // Para a execução caso escolha sair...
+                break;             // Suspende o Loop caso escolha SAIR...
             }
             else
             {
                 Console.WriteLine("Escolha uma opção válida...");
-            };
+            }
             Console.ReadKey();
         }
     }
 
+    // Listas para guardar os dados dos profissionais
 
-    // Lista para guardar os dados dos profissionais
-
-    static List<int> registros = new List<int>();
+    static List<int> ids = new List<int>();
     static List<string> nomes = new List<string>();
     static List<string> especialidades = new List<string>();
     static List<string> telefones = new List<string>();
@@ -83,67 +80,79 @@ class Program
         Console.WriteLine("Digite o Telefone:\n");
         string telefone = Console.ReadLine();
 
-        int registro = registros.Count + 1;
+        int id = ids.Count + 1;               // Atribui 1 cadastro(id) a cada nova interação. O id é usado para rastrear cada cadastro
 
-        registros.Add(registro);
-        nomes.Add(nome);
-        especialidades.Add(especialidade);
-        telefones.Add(telefone);
+        ids.Add(id);                          // Adiciona o que está na variável 'id' à lista 'ids'
+        nomes.Add(nome);                      // Adiciona o que está na variável 'nome' à lista 'nomes'
+        especialidades.Add(especialidade);    // Adiciona o que está na variável 'especialidade' à lista 'especialidades'
+        telefones.Add(telefone);              // Adiciona o que está na variável 'telefone' à lista 'telefones'
 
         Console.Clear();
-        Console.WriteLine("\nCadastro Concluído!\nTecle |C| para novo Cadastro ou |Enter| para voltar ao Menu");
+        Console.WriteLine("\nCadastro Concluído!\n" +
+        "Tecle |C| para novo Cadastro ou |Enter| para voltar ao Menu");
 
-        if (Console.ReadKey(true).Key == ConsoleKey.C)
+        while(true)                                     //Roda o bloco em Loop
         {
-            Cadastrar();
+            var tecla = Console.ReadKey(true).Key;      //Guarda a tecla pressionada
+
+            if (tecla == ConsoleKey.C)                  //Pede para teclar 'C'
+            {
+                Cadastrar();                            //Chama o menu de cadastro
+            }
+            else if (tecla == ConsoleKey.Enter)         //Pede para teclar 'Enter'
+            {
+                MenuPrincipal();                        //Chama o menu principal
+                break;
+            }
+            else 
+            {
+                Console.WriteLine("Opção inválida!");
+                break;
+            }
         }
-
-
     }
 
     // Método Listar
     static void Listar()
     {
         Console.Clear();
-        Console.WriteLine("\n===# Menu de Listagem #===");
+        Console.WriteLine("\n===# Menu de Listagem #===\n");
 
-        if (registros.Count < 1)
+        if (ids.Count < 1)
         {
-            Console.WriteLine("\nNão há Profissionais Listados...");
+            Console.WriteLine("Não há Profissionais Listados...");
         }
 
-        for (int i = 0; i < registros.Count; i++)
+        for (int i = 0; i < ids.Count; i++)
         {
-            Console.WriteLine($"\n------------------------\nRegistro: {registros[i]}\n");
+            Console.WriteLine("\n------------------------\n");
+            Console.WriteLine($"id: {ids[i]}\n");
             Console.WriteLine($"Nome: {nomes[i]}\n");
             Console.WriteLine($"Especialidade: {especialidades[i]}\n");
-            Console.WriteLine($"Telefone: {telefones[i]}\n------------------------");
+            Console.WriteLine($"Telefone: {telefones[i]}");
+            Console.WriteLine("\n------------------------\n");
 
         }
     }
 
-    //Método Atualizar
+    // Método Atualizar
     static void Atualizar()
     {
         Console.Clear();
         Console.WriteLine("\n===# Menu de Atualização #===\n");
 
-        if (registros.Count == 0)
+        for (int i = 0; i < ids.Count; i++) 
         {
-            Console.WriteLine("Não há profissionais para atualizar...");
-            return;
+            Console.WriteLine($"\nid: {i + 1}\nNome: {nomes[i]}");
         }
 
-        for (int i = 0; i < registros.Count; i++) 
-        {
-            Console.WriteLine($"{i + 1}. {registros[i]}");
-        }
-
-        Console.WriteLine("Digite o Registro do profissional que deseja atualizar");
+        Console.WriteLine("Digite o id do profissional que deseja atualizar");
         string entrada = Console.ReadLine();
 
-        if (int.TryParse(entrada, out int indiceLista) && indiceLista >= 1 && indiceLista <= registros.Count) 
+        if (int.TryParse(entrada, out int indiceLista) && indiceLista >= 1 && indiceLista <= ids.Count) 
         {
+            int idNovo = indiceLista;
+
             Console.WriteLine("\nDigite o novo Nome");
             string nomeNovo = Console.ReadLine();
 
@@ -153,9 +162,15 @@ class Program
             Console.WriteLine("\nDigite o novo Telefone");
             string telNovo = Console.ReadLine();
 
+            ids[indiceLista - 1] = idNovo;
             nomes[indiceLista - 1] = nomeNovo;
             especialidades[indiceLista - 1] = especNova;
             telefones[indiceLista - 1] = telNovo;
+        }
+        else if (ids.Count < 1)
+        {
+            Console.WriteLine("Não há profissionais para atualizar...");
+            return;
         }
     }
 
@@ -164,15 +179,16 @@ class Program
     {
         Console.Clear();
         Console.WriteLine("\n===# Menu de Exclusão #===\n");
-        if (registros.Count == 0)
+
+        if (ids.Count == 0)
         {
             Console.WriteLine("\nNão há Profissionais para Excluir...");
             return;
         }
 
-        Console.WriteLine("\nDigite o Registro do Profissional que deseja Excluir");
+        Console.WriteLine("\nDigite o id do Profissional que deseja Excluir");
 
-        if (int.TryParse(Console.ReadLine(), out int registro))
+        if (int.TryParse(Console.ReadLine(), out int id))
         {
 
         }
@@ -183,11 +199,11 @@ class Program
 
         bool encontrado = false;
 
-        for (int i = 0; i < registros.Count; i++)
+        for (int i = 0; i < ids.Count; i++)
         {
-            if (registros[i] == registro)
+            if (ids[i] == id)
             {
-                registros.RemoveAt(i);
+                ids.RemoveAt(i);
                 nomes.RemoveAt(i);
                 especialidades.RemoveAt(i);
 
