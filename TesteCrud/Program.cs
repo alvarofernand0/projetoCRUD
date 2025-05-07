8c3f6c78d6c0801d66ef9f8b8505e505ab87c997
Alvaro <alvarofernando1324@gmail.com>
2025-05-06 23:48:08 -0300

    Adicionando um teste e Ajustando pausas com ReadKey

---
+++ TesteCrud/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+Menu();
+
+void Menu()
+{
+    Console.WriteLine("=== MENU ===\n");
+    Console.WriteLine("1 - Cadastrar\n");
+    Console.WriteLine("2 - Listar\n");
+    Console.WriteLine("3 - Atualizar\n");
+    Console.WriteLine("4 - Excluir\n");
+    Console.WriteLine("5 - SAIR\n");
+    Console.WriteLine("");
+
+
+    if (Algo <1)
+    {
+        Console.WriteLine("Fiz algo errado");
+    }
+}
+
+void Cadastrar()
+{
+
+}
+
+void Listar()
+{
+
+}
+
+void Atualizar()
+{
+
+}
+
+void Excluir()
+{
+
+}
